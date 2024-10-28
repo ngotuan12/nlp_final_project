ff97cd2a6e6a9a4161facee993f76169e766230a
Ngo Anh Tuan <ngo.tuan12@gmail.com>
2024-10-28 11:28:00 +0700

    up

--- roberta.py
+++ roberta.py
@@ -18,3 +18,14 @@ tokenized_dataset = dataset.map(tokenize, batched=True)
 
 # Load model
 model = XLMRobertaForSequenceClassification.from_pretrained("xlm-roberta-base", num_labels=2)
+
+#training_args
+training_args = TrainingArguments(
+    output_dir="./results",
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    num_train_epochs=3,
+    weight_decay=0.01
+)
